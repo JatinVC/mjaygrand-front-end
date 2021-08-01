f73209dba8ed7c648cb3686592718c617d38b1f2
JatinVC <jchandwaney@gmail.com>
2021-08-01 16:16:08 +0800

    init commit

---
+++ precache-manifest.74831a34ebaba1df2c4d8c208269385e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "881c1503671083cf013e1cd678e82bab",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7748d23c4b12ef4165ff",
+    "url": "./static/css/main.0a43da34.chunk.css"
+  },
+  {
+    "revision": "3798120d5edea572acbd",
+    "url": "./static/js/2.03c4cb78.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.03c4cb78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7748d23c4b12ef4165ff",
+    "url": "./static/js/main.8ac19be2.chunk.js"
+  },
+  {
+    "revision": "ba3b5bd43745e7647455",
+    "url": "./static/js/runtime-main.7f1d4352.js"
+  },
+  {
+    "revision": "0d42873240dc84931477c3182e35b4a8",
+    "url": "./static/media/background.0d428732.jpg"
+  },
+  {
+    "revision": "4382fdde2b0f32e433981f2f901e7ce8",
+    "url": "./static/media/goals.4382fdde.jpg"
+  },
+  {
+    "revision": "1545403e15f9e18ab82c92df7a5dd4ac",
+    "url": "./static/media/logo.1545403e.png"
+  },
+  {
+    "revision": "ab5c3ae4021c5b9838353caec4ae1681",
+    "url": "./static/media/mission.ab5c3ae4.jpg"
+  },
+  {
+    "revision": "0dcb166fa54f28571c675a161e012dc3",
+    "url": "./static/media/purpose.0dcb166f.jpg"
+  },
+  {
+    "revision": "76b05d23ec08e6b21b2d52481aa42748",
+    "url": "./static/media/value.76b05d23.jpg"
+  },
+  {
+    "revision": "bbd8e41ae1c6dd45e563614025e57c94",
+    "url": "./static/media/vision.bbd8e41a.jpg"
+  }
+]);
